be0d508693ef61d04e2930ebf0e5b85252191be7
beliciabamal237 <bamalbelicia@gmail.com>
2022-07-01 06:14:47 -0700

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  *main - Entry point
  *the use function puts
- *Return: Always 0 (Success)
+ *Return: Always 0(Success)
  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		return (0);
 }
