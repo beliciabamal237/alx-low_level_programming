994f32589e5dadd6df52a0926243c3b732db0263
beliciabamal237 <bamalbelicia@gmail.com>
2022-07-01 03:20:20 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 
 /*
  *main - Entry point
- *this use function puts
+ *the use function puts
  *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
