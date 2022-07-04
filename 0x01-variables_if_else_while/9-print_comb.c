279ed9a38dd51ee56973b27513e8df320636db8c
beliciabamal237 <bamalbelicia@gmail.com>
2022-07-04 16:57:49 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,51 +1,25 @@
 #include <stdio.h>
 
-
-
-/**
- *
- *  * main- Entry point
- *
- *   * @void: Null value
- *
- *    *
- *
- *     * Description: Print numbers from 0 to 9 separated by comma and space
- *
- *      * Return: Zero value
- *
- *       */
-
-
+ /**
+  *main- Entry point
+  *@void: Null value
+  *Description: Print numbers from 0 to 9 separated by comma and space
+  *Return: Zero value
+  */
 
 int main(void)
-
 {
+	int n;
 
-		int n;
-
-
-
-			for (n = '0'; n <= '9'; n++)
-
-					{
-
-								putchar(n);
-
-										if (n != '9')
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		}
+		return (0);
 }
