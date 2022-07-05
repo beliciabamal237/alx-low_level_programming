0721ff6d1a0c632cab9991af25971eb1ebed8f7f
beliciabamal237 <bamalbelicia@gmail.com>
2022-07-05 05:40:28 -0700

    putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *the use function puts
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	char message[] = "_putchar";
+	int count = 0;
+
+
+	while (message[count] != '\0')
+	{
+		_putchar(message[count]);
+		count++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
