610e03f754583c86ff39fcdb42c8ae89b50cf91a
beliciabamal237 <bamalbelicia@gmail.com>
2022-10-13 05:05:07 -0700

    print text with puts fonction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Print exactly "Programming is like building a multilingual puzzle,
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
