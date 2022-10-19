75eea08b4a0af017025840d8e797b5139429bd1b
beliciabamal237 <bamalbelicia@gmail.com>
2022-10-19 07:33:48 -0700

    add number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and return the result
+ * @x: the int to add
+ * @y: the int to add
+ *
+ * Return: result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
