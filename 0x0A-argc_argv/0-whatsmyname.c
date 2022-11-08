f7e8389bd4c69d9f8a2c9850538906efae5828ab
beliciabamal237 <bamalbelicia@gmail.com>
2022-11-08 05:06:16 -0800

    print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: the count of argument
+ * @argv: the array
+ *
+ * Return: 0 success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+		{
+		printf("%s\n", *argv);
+		return (0);
+		}
+
