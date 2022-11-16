207af6d900cae80dfc128fa44efb29828784584f
beliciabamal237 <bamalbelicia@gmail.com>
2022-11-16 05:45:38 -0800

    no

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+int _putchar(char c);
 
 
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * struct Dog -  short description
  * @name: the dog'sname
@@ -17,6 +16,7 @@ struct Dog
 	float age;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
